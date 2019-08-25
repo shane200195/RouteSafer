414b5c4ff69a16478505f36e64636ea895f74a83
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-08-25 06:45:32 -0400

    added static frontend interface

--- FrontEnd/routesafer-frontend/src/App.js
+++ FrontEnd/routesafer-frontend/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-
 import Home from './Home';
 import Map from './Map';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <Map />
+      <BrowserRouter>
+          <switch>
+            <Route exact path='/'  component={Home} />
+            <Route exact path='/map' component={Map} />
+          </switch>
+        </BrowserRouter>
     </div>
   );
 }
