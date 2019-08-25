77a650fecb98ce3ad3a59d9812b9506abb9b27e5
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-08-25 01:02:40 -0400

    added navbar

---
+++ FrontEnd/routesafer-frontend/src/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Navbar;
